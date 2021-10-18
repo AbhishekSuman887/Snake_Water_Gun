9a5062e598094d64cd42b50cbdcf007d137b9930
Abhishek <avi.suman887@gmail.com>
2021-10-18 18:50:57 +0530

    Initial Commit

---
+++ Snake_Water_Gun_Game.py
@@ -0,0 +1,46 @@
+import random
+
+play = "Yes"
+
+def Game(c, p):
+    if(c == p):
+        return None
+    if(c == 'g'):
+        if(p == 's'):
+            return False
+        if(p == 'w'):
+            return True
+    if(c == 'w'):
+        if(p == 'g'):
+            return False
+        if(p == 's'):
+            return True
+    if(c == 's'):
+        if(p == 'w'):
+            return False
+        if(p == 'g'):
+            return True
+         
+      
+while(play!='q'):
+    print("*********************************************")
+    print("Computer is Choosing...")
+    i = random.randint(1, 3)
+    print("Computer has Choosen!")
+    if (i == 1):
+        comp = "s"
+    if (i == 2):
+        comp = "w"
+    if (i == 3):
+        comp = "g"
+    player = input("Your turn to Choose :\n1.For Choosing Snake - Press 's'\n2.For Chhosing Water - Press 'w'\n3.For Choosing Gun   - Press 'g'\n")
+    result = Game(comp,player)
+    print(f"You have Choosed '{player}' and Computer had Choosen '{comp}'")
+    if result == None:
+        print("The Game was Draw as you both had choosen the same.\n")
+    if result == True:
+        print("You Won!\n")
+    if result == False:
+        print("You Lost!\n")
+    play = input("Enter 'q' if you want to Quit playing!\nIf you want to play more hit Enter!\n")
+
